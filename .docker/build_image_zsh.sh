1bab802e7012fcabd644e978b31d1021edbae43d
vinceHuyghe <vinceHuyghe@users.noreply.github.com>
2023-02-06 16:05:53 +0000

    dockerfile

--- .docker/build_image_zsh.sh
+++ .docker/build_image_zsh.sh
@@ -2,8 +2,13 @@
 
 echo -e "Building moveit1_ur:lastest image"
 
+# DOCKER_BUILDKIT=1 \
+# docker build --pull --rm -f ./.docker/Dockerfile \
+# --build-arg BUILDKIT_INLINE_CACHE=1 \
+# --target zsh \
+# --tag moveit1_ur:latest .
+
 DOCKER_BUILDKIT=1 \
 docker build --pull --rm -f ./.docker/Dockerfile \
---build-arg BUILDKIT_INLINE_CACHE=1 \
 --target zsh \
 --tag moveit1_ur:latest .
